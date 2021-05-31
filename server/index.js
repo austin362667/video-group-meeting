668385f905e377188ef67a76f1641ed38719ba0f
Austin <austin362667@gmail.com>
2021-05-31 15:46:25 +0800

    init

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 // const http = require('http').createServer(app);
-const io = require('socket.io')(http);
 const PORT = process.env.PORT || 443;
 const path = require('path');
 
@@ -16,6 +15,7 @@ var options = {
 
 
 const server = https.createServer(options, app);
+const io = require('socket.io')(server);
 
 
 
